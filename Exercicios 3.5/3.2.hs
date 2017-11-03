9fd424bae8682378f520be94d2b2aba1850a8eb3
Vinicius Falcão <vinyfalcao@hotmail.com>
2017-11-03 11:10:08 -0300

    ultimo commit, no momento estou estudando por outro livro

--- Exercicios 3.5/3.2.hs
+++ Exercicios 3.5/3.2.hs
@@ -13,16 +13,17 @@ converterCelsius graus Celsius = graus
 --converterKelvin	 :	recebe	um	valor	 	double	 	e	uma
 --temperatura,	e	faz	a	conversão	para	Kelvin.
 converterKelvin :: Double -> Temperatura -> Double
-converterCelsius graus Farenheit = (graus - 32) * (5/9)
-converterCelsius graus Kelvin = graus - 273.15
-converterCelsius graus Celsius = graus
-
+converterKelvin graus Farenheit = converterCelsius (converterCelsius graus Farenheit) Celsius
+converterKelvin graus Kelvin = graus
+converterKelvin graus Celsius = graus + 273.15
 
 
 --converterFarenheit	 :	 recebe	 um	 valor	 	double	 	 e
 --uma	temperatura,	e	faz	a	conversão	para	Farenheit.
-
-
+converterFarenheit :: Double -> Temperatura -> Double
+converterFarenheit graus Farenheit = graus
+converterFarenheit graus Kelvin = converterFarenheit (converterCelsius graus Kelvin) Celsius
+converterFarenheit graus Celsius =  (graus * 1.8) +32
 
 main :: IO ()
 main = print Celsius
